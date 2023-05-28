497bc23917e550e453a993995230817ae910ccae
Romina Manzano <manzano.romina13@gmail.com>
2023-05-28 19:14:27 -0300

    ADD: initial footer test

---
+++ src/components/__tests__/Footer.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from '../Footer';
+
+describe('<Footer />', () => {
+  it('renders footer text', () => {
+    const { getByText } = render(<Footer />);
+    const currentYear = new Date().getFullYear();
+    expect(getByText(`© ${currentYear} Romina Manzano`)).toBeInTheDocument();
+  });
+});
