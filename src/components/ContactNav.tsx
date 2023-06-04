f5e0ac1b7e04e5967725f7ebb04a3a1368b4f954
Romina Manzano <manzano.romina13@gmail.com>
2023-06-04 20:15:45 -0300

    ADD: ContactNav tests

--- src/components/ContactNav.tsx
+++ src/components/ContactNav.tsx
@@ -5,29 +5,26 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const githubUrl = process.env.NEXT_PUBLIC_GITHUB_PROFILE_URL || '';
 const linkedinUrl = process.env.NEXT_PUBLIC_LINKEDIN_PROFILE_URL || '';
+const iconSize = 32;
 
-const ContactNav = () => {
-  const iconSize = 32;
+const ContactNav = () => (
+  <nav className="flex justify-end">
+    <div className="flex justify-end p-4 gap-2 w-3/12 border-b-2 border-light-coral">
+      <Link href={githubUrl} target="_blank" aria-label="GitHub">
+        <FaGithub
+          size={iconSize}
+          className="text-light-coral"
+        />
+      </Link>
 
-  return (
-    <nav className="flex justify-end">
-      <div className="flex justify-end p-4 gap-2 w-3/12 border-b-2 border-light-coral">
-        <Link href={githubUrl} target="_blank">
-          <FaGithub
-            size={iconSize}
-            className="text-light-coral"
-          />
-        </Link>
-
-        <Link href={linkedinUrl} target="_blank">
-          <FaLinkedin
-            size={iconSize}
-            className="text-light-coral"
-          />
-        </Link>
-      </div>
-    </nav>
-  );
-};
+      <Link href={linkedinUrl} target="_blank" aria-label="LinkedIn">
+        <FaLinkedin
+          size={iconSize}
+          className="text-light-coral"
+        />
+      </Link>
+    </div>
+  </nav>
+);
 
 export default ContactNav;
---
+++ src/components/__tests__/ContactNav.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+
+import ContactNav from '../ContactNav';
+
+describe('ContactNav', () => {
+  it('renders two clickable social media icons', () => {
+    const { getByRole } = render(<ContactNav />);
+
+    const githubIcon = getByRole('link', { name: 'GitHub' });
+    expect(githubIcon).toBeVisible();
+    userEvent.click(githubIcon);
+
+    const linkedinIcon = screen.getByRole('link', { name: 'LinkedIn' });
+    expect(linkedinIcon).toBeVisible();
+    userEvent.click(linkedinIcon);
+  });
+});
