a4f0795158924d4e47b296430b2345f320e4b7df
Romina Manzano <manzano.romina13@gmail.com>
2024-05-07 14:40:36 -0300

    UPDATE: setup chakra with storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import { theme } from "@chakra-ui/react";
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    chakra: {
+      theme,
+    },
+  },
+};
+
+export default preview;
---
+++ src/components/organisms/ProfileCard.tsx
@@ -0,0 +1,11 @@
+import { Card, CardBody } from "@chakra-ui/react";
+
+const ProfileCard = () => {
+  return (
+    <Card>
+      <CardBody>ProfileCard</CardBody>
+    </Card>
+  );
+};
+
+export default ProfileCard;
