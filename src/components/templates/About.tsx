d24998cc602e87843fa7a6535543ceff59b1b73a
Romina Manzano <manzano.romina13@gmail.com>
2024-05-08 15:21:59 -0300

    ADD: scroll and css adjustments

--- src/components/templates/About.tsx
+++ src/components/templates/About.tsx
@@ -3,12 +3,14 @@ import profilePicture from "../../images/RominaManzano.jpg";
 import ProfileCard from "../organisms/ProfileCard";
 
 const About = () => (
-  <ProfileCard
-    name="Romina Manzano"
-    title="Systems Engineer"
-    infoItems={infoItems}
-    image={profilePicture.src}
-  />
+  <div id="about">
+    <ProfileCard
+      name="Romina Manzano"
+      title="Systems Engineer"
+      infoItems={infoItems}
+      image={profilePicture.src}
+    />
+  </div>
 );
 
 export default About;
--- src/components/templates/Experience.tsx
+++ src/components/templates/Experience.tsx
@@ -10,12 +10,12 @@ import ProjectCard from "../organisms/ProjectCard";
 import TimeLine from "../organisms/TimeLine";
 
 const Experience = () => (
-  <Stack>
+  <Stack id="experience">
     <Heading as="h1" size="lg" mb={2}>
       Experience
     </Heading>
 
-    <Heading as="h2" size="md" mb={4}>
+    <Heading as="h2" size="md" mb={4} id="professional-experience">
       Professional Experience
     </Heading>
 
@@ -23,11 +23,11 @@ const Experience = () => (
 
     <Divider my={4} color="gray.600" borderWidth="1px" />
 
-    <Heading as="h2" size="md" mb={4}>
+    <Heading as="h2" size="md" mb={4} id="freelance-personal-projects">
       Freelance/Personal Projects
     </Heading>
 
-    <SimpleGrid spacing={4} templateColumns="repeat(2, minmax(0px, 1fr))">
+    <SimpleGrid spacing={4} templateColumns="repeat(2, minmax(300px, 1fr))">
       {personalExperienceItems.map((item) => (
         <GridItem key={item.title} display="flex" justifyContent="center">
           <ProjectCard {...item} />
